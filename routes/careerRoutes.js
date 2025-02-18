98abe463dbb926fdad4848b4d4076856c6b4cca2
GitShailendra <kumarshailrndra7355@gmail.com>
2025-02-18 12:16:48 +0530

    middlewares added

--- routes/careerRoutes.js
+++ routes/careerRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const careerController = require('../controllers/careerController');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
+const {verifyToken} = require('../middlewares/adminMiddleware');
 
 router.post('/submit', upload.single('resume'), careerController.submitApplication);
-router.get('/applications', careerController.getApplications);
+router.get('/applications',verifyToken, careerController.getApplications);
 // router.get('/applications/:id', getApplication);
-router.get('/applications/:id/resume', careerController.downloadResume);
+router.get('/applications/:id/resume',verifyToken, careerController.downloadResume);
 // router.get('/applications', applicationController.getApplications);
 
 module.exports = router;
